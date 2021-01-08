ef94062487b7100d80e6e3aece762bdfde6d333a
ayano kobayashi <ultrakobayashiayano@yahoo.co.jp>
2021-01-08 23:11:24 +0900

    CRUD機能作成

---
+++ db/migrate/20210108082730_create_properties.rb
@@ -0,0 +1,11 @@
+class CreateProperties < ActiveRecord::Migration[5.2]
+  def change
+    create_table :properties do |t|
+      t.string "name"
+      t.integer "rent"
+      t.string "address"
+      t.integer "age"
+      t.text "content"
+    end
+  end
+end
